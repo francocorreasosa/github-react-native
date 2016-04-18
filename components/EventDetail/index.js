3b8a356fad1abc25c909da6eb351062703c9b427
Franco Correa <franco@francocorrea.com.uy>
2016-04-17 23:01:47 -0300

    Add event details (navigation on iOS)

--- components/EventDetail/index.js
+++ components/EventDetail/index.js
@@ -2,14 +2,36 @@
 
 import React, {
   Text,
+  View,
+  Image
 } from 'react-native';
+import moment from 'moment';
 
 import styles from './styles';
 
 class EventDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      event: this.props.event
+    }
+  }
   render() {
+
     return (
-      <Text>hello EventDetail</Text>
+      <View style={styles.container}>
+        <Image
+          source={{uri: this.state.event.actor.avatar_url}}
+          style={styles.actorImage}
+        />
+
+        <Text style={styles.createdAt}>
+          {moment(this.state.event.created_at).format('ll')}
+        </Text>
+
+        <Text style={styles.createdAt}>{this.state.event.actor.login}</Text>
+        <Text>created {this.state.event.repo.name}</Text>
+      </View>
     );
   }
 }
--- components/EventDetail/styles.js
+++ components/EventDetail/styles.js
@@ -5,4 +5,20 @@ import React, {
 } from 'react-native';
 
 module.exports = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 80,
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  actorImage: {
+    height: 120,
+    width: 120,
+    borderRadius: 15
+  },
+  createdAt: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    fontSize: 20
+  }
 });
